a54712190c269644f991835632c478af87e1f822
azuborev <azuborev@corp.web4pro.com.ua>
2020-09-10 14:04:26 +0300

    exercise4

--- exercise4/DatabaseClass.php
+++ exercise4/DatabaseClass.php
@@ -14,10 +14,9 @@ class Database
         try{
             $dsn = "mysql:dbname=" . $this->dbname . ";host=" . $this->dbhost;
             $this->db_connection = new PDO($dsn, $this->dbuser, $this->dbpass);
+            $this->db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }catch(PDOException $e){
-
             echo 'Подключение не удалось: ' . $e->getMessage();
-
         }
     }
 
@@ -38,21 +37,4 @@ class Database
         return $this->db_connection;
     }
 }
-
-
-    $dbh = Database::getInstance()->connect();
-    // Создание таблицы
-    $sql = "CREATE TABLE Students (
-                id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                firstName VARCHAR(30) NOT NULL,
-                secondName VARCHAR(30) NOT NULL,
-                age TINYINT(3) NOT NULL,
-                gender ENUM('m', 'w') NOT NULL,
-                phone VARCHAR(11) NOT NULL,
-                email VARCHAR(50) NOT NULL,
-                course VARCHAR(30) NOT NULL,
-                university VARCHAR(30) NOT NULL,
-                studentship INT(5) DEFAULT 0)
-                ";
-    $dbh->exec($sql);
 
