2088610aaa4dff71b341af2c8fbe56c471f20ace
azuborev <azuborev@corp.web4pro.com.ua>
2020-09-22 18:21:04 +0300

    register scripts

--- wp/wp-content/plugins/zb-search-post/class-zb-search-widget.php
+++ wp/wp-content/plugins/zb-search-post/class-zb-search-widget.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 class ZB_Search_Widget extends WP_Widget
 {
     public function __construct()
@@ -11,12 +9,17 @@ class ZB_Search_Widget extends WP_Widget
             'description' => __( 'You can search posts by date or title' ),
         ];
         parent::__construct('zb-search-widget', 'Post-Search', $args);
+
+        if ( is_active_widget( false, false, $this->id_base ) || is_customize_preview() ) {
+            add_action('wp_register_scripts', array( $this, 'add_genius_scripts' ));
+            add_action('wp_head', array( $this, 'add_genius_style' ) );
+        }
     }
 
     public function form($instance)
     {
         $title = ( ! empty( $instance['title'] )) ? $instance['title'] : '';
-        $post_number = ( ! empty( $instance['post_number'] )) ? $instance['post_number'] : '';
+        $post_number = ( ! empty( $instance['post_number'] )) ? $instance['post_number'] : '5';
 ?>
         <p>
             <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title' ) ?></label>
@@ -25,23 +28,29 @@ class ZB_Search_Widget extends WP_Widget
         </p>
             <p>
                 <label for="<?php echo $this->get_field_id('post_number'); ?>"><?php _e( 'Number of posts:' ) ?></label>
-                <input class="widefat" id="<?php echo $this->get_field_id('post_number'); ?>" type="text"
-                       name="<?php echo $this->get_field_name('post_number'); ?>" value="<?php echo esc_attr( $post_number ); ?>">
+                <input id="<?php echo $this->get_field_id('post_number'); ?>" type="text"
+                       name="<?php echo $this->get_field_name('post_number'); ?>"
+                       value="<?php echo esc_attr( $post_number ); ?>" size="3">
             </p>
 <?php
     }
 
     public function widget($args, $instance)
     {
-        $count = ( $instance['event_number'] ) ? $instance['event_number'] : '';
-        $status = ( $instance['event_status'] ) ? $instance['event_status'] : '';
+        wp_enqueue_script('jq-hoverintent', 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', ['jquery'],false,true );
+
+        $number = ( $instance['post_number'] );
+        $title = apply_filters( 'widget_title', $instance['title'] );
 
         echo $args['before_widget'];
-        echo $args['before_title'];
-        echo $instance['event_title'];
-        echo $args['after_title'];
-        echo zb_show_events_front_widget($status, $count);
+        if ( ! empty( $title ) ) {
+            echo $args['before_title'] . $title . $args['after_title'];
+        }
+        echo get_form();
         echo $args['after_widget'];
+
+
+//        echo zb_show_events_front_widget($status, $count);
     }
 
 
@@ -49,8 +58,37 @@ class ZB_Search_Widget extends WP_Widget
     {
         $instance = [];
         $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-        $instance['post_number'] =( intval( $new_instance['post_number'] ) ) ?
-                                    strip_tags( $new_instance['post_number'] ) : $old_instance['post_number'] ;
+        $instance['post_number'] =( is_numeric( $new_instance['post_number'] ) ) ?
+                                    strip_tags( $new_instance['post_number'] ) : '5' ;
         return $instance;
     }
 }
+
+function get_form() {
+    $form = '';
+    $form .= '<p><input type="text" name="title" id="title" placeholder="Post\'s Title"></p>';
+    $form .= '<p><input type="date" name="from_date" id="from_date"</p>';
+    $form .= '<p><input type="submit" name="search_submit" id="search_submit"</p>';
+    return $form;
+}
+//jQuery(document).ready( function() {
+//    jQuery(".user_like").click( function(e) {
+//        e.preventDefault();
+//        post_id = jQuery(this).attr("data-post_id");
+//        nonce = jQuery(this).attr("data-nonce");
+//        jQuery.ajax({
+//         type : "post",
+//         dataType : "json",
+//         url : myAjax.ajaxurl,
+//         data : {action: "my_user_like", post_id : post_id, nonce: nonce},
+//         success: function(response) {
+//            if(response.type == "success") {
+//                jQuery("#like_counter").html(response.like_count);
+//            }
+//            else {
+//                alert("Your like could not be added");
+//            }
+//        }
+//      });
+//   });
+//});
--- wp/wp-content/plugins/zb-search-post/zb-search.php
+++ wp/wp-content/plugins/zb-search-post/zb-search.php
@@ -4,10 +4,21 @@
 * Description: Plugin adds widget for posts search
 */
 
-require __DIR__ . '/class-zb-search-widget.php';
+require_once __DIR__ . '/class-zb-search-widget.php';
 
 add_action('widgets_init', 'zb_search_widget');
-
 function zb_search_widget() {
     register_widget('Zb_Search_Widget');
 }
+
+add_action( 'wp_enqueue_scripts', 'load_scripts' );
+function load_scripts() {
+    if ( ! wp_script_is( 'jquery', 'enqueued' )) {
+        wp_deregister_script( 'jquery' );
+        wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', [],false,true );
+    }
+    wp_register_script('search_ajax.js', plugins_url('.search_ajax.js', __FILE__), ['jquery'], false, true );
+    wp_localize_script('xz-favorites-scripts', 'xzFavorites', ['url' => admin_url('admin-ajax.php'), 'nonce' => wp_create_nonce('xz-favorites'), 'postId'=>$post->ID] );
+}
+
+https://www.youtube.com/watch?v=DNCPX5uuUBk
